638f70e10ddd448f6fe9c772ff05d553e416648b
dhwan <ehdghks9719@naver.com>
2024-09-02 22:21:57 +0900

    스프링 자동의존관계주입

---
+++ spring-basic/core/core/src/main/java/hello/core/HelloLombok.java
@@ -0,0 +1,20 @@
+package hello.core;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloLombok {
+
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok hello = new HelloLombok();
+        hello.setName("Lombok");
+
+        String name = hello.getName();
+        System.out.println("name = " + name);
+    }
+}
--- spring-basic/core/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring-basic/core/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,11 +1,14 @@
 package hello.core.order;
 
 import com.sun.source.tree.UsesTree;
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,9 +17,8 @@ public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        System.out.println("memberRepository = " + memberRepository);
-        System.out.println("discountPolicy = " + discountPolicy);
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
